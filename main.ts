203b94c83d42bd6626cbe4f5920d3a6aebf6faf8
421385 <115143096+unknown-creatur@users.noreply.github.com>
2022-10-20 16:55:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,35 @@
+function light_numbers (num: number) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 10) {
+        x = 4
+        y = num - 5
+    } else if (num >= 10 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num < 17) {
+        x = 0
+        y = 17 - num
+    } else if (num >= 17 && num < 20) {
+        x = num - 16
+        y = 1
+    } else if (num >= 20 && num < 22) {
+    	
+    } else {
+    	
+    }
+    led.plot(x, y)
+}
+let a = 0
+let y = 0
+let x = 0
+let num = 0
+led.plot(0, 0)
+num = 0
+basic.forever(function () {
+    a += 1
+    num += 1
+    light_numbers(a)
+    basic.pause(100)
+})
